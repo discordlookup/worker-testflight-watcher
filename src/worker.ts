bc619f3ab051439d092817bf6dde77c8f5277ead
Felix <github@fbrettnich.de>
2023-06-10 21:10:57 +0200

    Updated embed
    Signed-off-by: Felix <github@fbrettnich.de>

--- src/worker.ts
+++ src/worker.ts
@@ -1,20 +1,42 @@
-const seatsAvailableAgain = () => ({
-  title: "Discord TestFlight",
-  url: "https://dis.gd/testflight",
-  type: "rich",
-  description: "New seats available in Discord TestFlight (Discord iOS Beta)",
-  color: 5763719,
-  timestamp: new Date().toISOString(),
+const seatsAvailableAgain = (env: Env) => ({
+	content: "<@&1116109018247733401>",
+	embeds: [
+		{
+			title: "Discord TestFlight",
+			url: env.TESTFLIGHT_URL,
+			type: "rich",
+			description: "New Discord TestFlight (Discord iOS Beta) seats available!",
+			color: 5763719,
+			timestamp: new Date().toISOString(),
+		}
+	],
+	components: [
+		{
+			type: 1,
+			components: [
+				{
+					type: 2,
+					label: "Join TestFlight",
+					style: 5,
+					url: env.TESTFLIGHT_URL
+				}
+			]
+		}
+	]
 });
 
-const seatsFull = () => ({
-  title: "Discord TestFlight",
-  url: "https://dis.gd/testflight",
-  type: "rich",
-  description:
-    "All seats in Discord TestFlight (Discord iOS Beta) are taken again.",
-  color: 15548997,
-  timestamp: new Date().toISOString(),
+const seatsFull = (env: Env) => ({
+	embeds: [
+		{
+			title: "Discord TestFlight",
+			url: env.TESTFLIGHT_URL,
+			type: "rich",
+			description:
+				"All Discord TestFlight (Discord iOS Beta) seats are taken again.",
+			color: 15548997,
+			timestamp: new Date().toISOString(),
+		}
+	]
 });
 
 export default {
@@ -28,6 +50,7 @@ export default {
       (await env.TESTFLIGHT_STATE.get("TIME")) ?? new Date(0).toISOString();
     return new Response(
       JSON.stringify({
+				url: env.TESTFLIGHT_URL,
         state: currentState,
         lastUpdate,
       }),
@@ -40,7 +63,7 @@ export default {
   },
 
   async scheduled(event: Request, env: Env, ctx: ExecutionContext) {
-    const result = await fetch("https://testflight.apple.com/join/gdE4pRzI", {
+    const result = await fetch(env.TESTFLIGHT_URL, {
       headers: {
         "Accept-Language": "en-us",
       },
@@ -63,7 +86,7 @@ export default {
             "Content-Type": "application/json",
           },
           body: JSON.stringify(
-            newState === "FULL" ? seatsFull() : seatsAvailableAgain()
+            newState === "FULL" ? seatsFull(env) : seatsAvailableAgain(env)
           ),
         });
     }
--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -1,4 +1,5 @@
 interface Env {
 	TESTFLIGHT_STATE: KVNamespace;
+	TESTFLIGHT_URL?: string;
 	DISCORD_WEBHOOK_URL?: string;
 }
